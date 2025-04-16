e899d698c7452bff72b90558dc4dec5dcc55c396
HimankV <himank10rise@gmail.com>
2025-04-16 11:50:16 +0530

    changes

--- vite-project/src/Components/Register_Login.jsx
+++ vite-project/src/Components/Register_Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import image from "../assets/user2.png";
 
 const LoginForm = () => {
   const [isRegistering, setIsRegistering] = useState(false);
@@ -7,7 +8,7 @@ const LoginForm = () => {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [dob, setDob] = useState("");
-  const navigate = useNavigate(); // Use useNavigate hook
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     const userData = {
@@ -29,9 +30,9 @@ const LoginForm = () => {
       const data = await response.json();
       if (response.ok) {
         localStorage.setItem("token", data.token);
-        localStorage.setItem("user", JSON.stringify(data.user)); // Store user info
+        localStorage.setItem("user", JSON.stringify(data.user));
         alert("Success!");
-        navigate("/users"); // Redirect to /users
+        navigate("/users");
       } else {
         alert(data.message || "Error occurred");
       }
@@ -39,6 +40,25 @@ const LoginForm = () => {
       console.error("Error:", error);
       alert("Network error");
     }
+  };
+
+  const inputStyle = {
+    width: "100%",
+    padding: "10px 10px 10px 30px",
+    borderRadius: "5px",
+    border: "none",
+    backgroundColor: "#35425e",
+    color: "#ccc",
+    fontSize: "14px",
+    boxSizing: "border-box", // Added box-sizing
+  };
+
+  const iconStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "5px",
+    fontSize: "16px",
+    color: "#aaa",
   };
 
   return (
@@ -61,6 +81,7 @@ const LoginForm = () => {
           boxShadow: "0 15px 40px rgba(0, 0, 0, 0.3)",
           position: "relative",
           color: "white",
+          overflow: "hidden",
         }}
       >
         <div
@@ -81,34 +102,71 @@ const LoginForm = () => {
           {isRegistering ? "SIGN UP" : "SIGN IN"}
         </div>
 
+        {/* Enhanced Curved Background Decoration */}
+        <svg
+          viewBox="0 0 350 200"
+          preserveAspectRatio="none"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "160px",
+            zIndex: 0,
+          }}
+        >
+          <path d="M0,0 C70,90 280,0 350,90 L350,0 L0,0 Z" fill="#283759" />
+          <path
+            d="M0,30 C90,120 260,20 350,110 L350,0 L0,0 Z"
+            fill="#2d3c5e"
+            opacity="0.6"
+          />
+          <path
+            d="M0,60 C120,150 230,50 350,140 L350,0 L0,0 Z"
+            fill="#32406b"
+            opacity="0.4"
+          />
+          <path
+            d="M0,90 C140,180 200,80 350,170 L350,0 L0,0 Z"
+            fill="#374477"
+            opacity="0.3"
+          />
+        </svg>
+
         <div
           style={{
             backgroundColor: "#2e3a5a",
             borderRadius: "50%",
             width: "80px",
             height: "80px",
-            margin: "30px auto 20px",
+            margin: "60px auto 20px",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             fontSize: "40px",
             color: "#888",
+            zIndex: 1,
+            position: "relative",
           }}
         >
-          👤
+          <img style={{ height: "50px" }} src={image} alt="" />
         </div>
 
         <div style={{ marginBottom: "15px", position: "relative" }}>
           <label
-            style={{ fontSize: "12px", marginBottom: "5px", display: "block" }}
+            style={{
+              fontSize: "12px",
+              marginBottom: "5px",
+              display: "block",
+            }}
           >
             Username
           </label>
           <span
             style={{
               position: "absolute",
-              top: "36px",
-              left: "10px",
+              top: "45%",
+              left: "5px",
               fontSize: "16px",
               color: "#aaa",
             }}
@@ -118,17 +176,9 @@ const LoginForm = () => {
           <input
             type="text"
             placeholder="username"
+            style={inputStyle}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            style={{
-              width: "100%",
-              padding: "10px 10px 10px 30px",
-              borderRadius: "5px",
-              border: "none",
-              backgroundColor: "#35425e",
-              color: "#ccc",
-              fontSize: "14px",
-            }}
           />
         </div>
 
@@ -147,17 +197,9 @@ const LoginForm = () => {
               <input
                 type="email"
                 placeholder="email"
+                style={inputStyle}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                style={{
-                  width: "100%",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  border: "none",
-                  backgroundColor: "#35425e",
-                  color: "#ccc",
-                  fontSize: "14px",
-                }}
               />
             </div>
 
@@ -173,17 +215,9 @@ const LoginForm = () => {
               </label>
               <input
                 type="date"
+                style={inputStyle}
                 value={dob}
                 onChange={(e) => setDob(e.target.value)}
-                style={{
-                  width: "100%",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  border: "none",
-                  backgroundColor: "#35425e",
-                  color: "#ccc",
-                  fontSize: "14px",
-                }}
               />
             </div>
           </>
@@ -191,15 +225,19 @@ const LoginForm = () => {
 
         <div style={{ marginBottom: "15px", position: "relative" }}>
           <label
-            style={{ fontSize: "12px", marginBottom: "5px", display: "block" }}
+            style={{
+              fontSize: "12px",
+              marginBottom: "5px",
+              display: "block",
+            }}
           >
             Password
           </label>
           <span
             style={{
               position: "absolute",
-              top: "36px",
-              left: "10px",
+              top: "45%",
+              left: "5px",
               fontSize: "16px",
               color: "#aaa",
             }}
@@ -209,17 +247,9 @@ const LoginForm = () => {
           <input
             type="password"
             placeholder="password"
+            style={inputStyle}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={{
-              width: "100%",
-              padding: "10px 10px 10px 30px",
-              borderRadius: "5px",
-              border: "none",
-              backgroundColor: "#35425e",
-              color: "#ccc",
-              fontSize: "14px",
-            }}
           />
         </div>
 
@@ -237,14 +267,19 @@ const LoginForm = () => {
               <input type="checkbox" style={{ marginRight: "5px" }} />
               Remember me
             </label>
-            <a href="#" style={{ color: "#00f5f9", textDecoration: "none" }}>
+            <a
+              href="#"
+              style={{
+                color: "#00f5f9",
+                textDecoration: "none",
+              }}
+            >
               Forgot your password?
             </a>
           </div>
         )}
 
         <button
-          onClick={handleSubmit}
           style={{
             backgroundColor: "#00f5f9",
             border: "none",
@@ -256,6 +291,7 @@ const LoginForm = () => {
             cursor: "pointer",
             marginBottom: "10px",
           }}
+          onClick={handleSubmit}
         >
           {isRegistering ? "REGISTER" : "LOGIN"}
         </button>
